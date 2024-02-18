a9ed5e2f6a2b7c31fca213f570a7e45aee309d6d
gaurav3247 <90786427+gaurav3247@users.noreply.github.com>
2024-02-18 05:39:18 -0500

    Update background.ts

--- Frontend/src/background/background.ts
+++ Frontend/src/background/background.ts
@@ -1,1 +1,21 @@
+import { GoogleSafeBrowse } from "../../../PhishNet-apiWrapper/API/GoogleSafeBrowse/mainCaller";
 
+console.log('Background script running');
+chrome.webNavigation.onBeforeNavigate.addListener(async function(details) {
+    const GSBAPIKey = 'AIzaSyCuXSnFYY-Q8hmphsEno9-tdJch-HtCdc8';
+
+    const GSBCaller = new GoogleSafeBrowse(GSBAPIKey);
+
+    const isMalicious = await GSBCaller.isMalicious(details.url);
+    if (isMalicious) {
+        console.log('This is a malicious website');
+        chrome.action.setBadgeBackgroundColor({color: 'red'});
+        chrome.action.setBadgeTextColor({color: 'black'});
+        chrome.action.setBadgeText({text: 'WARN'});
+    } else {
+        console.log('This is a safe website');
+        chrome.action.setBadgeBackgroundColor({color: 'green'});
+        chrome.action.setBadgeTextColor({color: 'white'});
+        chrome.action.setBadgeText({text: 'SAFE'});
+    }
+});
